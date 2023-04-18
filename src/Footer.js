b7f0dbfbed2f5cd511cd6bc1d7c3f27c66c3368d
Chaltuteklu12 <tedigirmaabebe@gmail.com>
2023-04-18 02:09:21 -0700

    First commit

---
+++ src/Footer.js
@@ -0,0 +1,80 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-ctnt">
+        <div className="contact-us">
+          <h2>
+            <a href="##">Questions?Contact us.</a>
+          </h2>
+        </div>
+        <div className="footer__links">
+          <ul>
+            <li>
+              <a href="#">FAQ</a>
+            </li>
+            <li>
+              <a href="#">Investor Relations</a>
+            </li>
+            <li>
+              <a href="#">Privacy</a>
+            </li>
+            <li>
+              <a href="#">speed test</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a href="#">Help Center</a>
+            </li>
+            <li>
+              <a href="#">Jobs</a>
+            </li>
+            <li>
+              <a href="#">Cookie Preferences</a>
+            </li>
+            <li>
+              <a href="#">Legal Notices</a>
+            </li>
+          </ul>
+
+          <ul>
+            <li>
+              <a href="#">Account</a>
+            </li>
+            <li>
+              <a href="#">Ways to Watch</a>
+            </li>
+            <li>
+              <a href="#">Corporate Information</a>
+            </li>
+            <li>
+              <a href="#">Only on Netflix</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a href="#">Media Center</a>
+            </li>
+            <li>
+              <a href="#">Terms of Use</a>
+            </li>
+            <li>
+              <a href="#">contact us</a>
+            </li>
+          </ul>
+        </div>
+        <div className="nflx-location">
+          <h2>Netflix Ethiopia</h2>
+        </div>
+        <h1>
+          Built By: <a href="https://www.Aschu24.com/">Aschalew Gashaw</a>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/Row.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+const Row = ({ Title, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      //   console.log(request);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  // console.log(movies);
+  const opts = {
+    heighr: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.title || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+  return (
+    <div className="row">
+      <h2>{Title}</h2>
+      <div className="row__posters">
+        {movies.map((singleMovie) => (
+          <img
+            key={singleMovie.id}
+            onClick={() => handleClick(singleMovie)}
+            className={`row__poster ${isLargeRow && "row__posterLarge"} `}
+            src={`${base_url}${
+              isLargeRow ? singleMovie.poster_path : singleMovie.backdrop_path
+            }`}
+            alt={singleMovie.name}
+          />
+        ))}
+      </div>
+      <div style={{ padding: "40px" }}>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
